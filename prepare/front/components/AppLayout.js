e97777ed438bc2557a11774a721c66f2943c7d8d
diki816 <dikisun@gmail.com>
2020-08-29 23:50:10 +0900

    redux first

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useState, useCallback, useMemo} from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
-import { Menu } from 'antd';
+import { Menu, Input, Row, Col } from 'antd';
+import styled from 'styled-components';
+import { useSelector } from 'react-redux';
+
+import LoginForm from '../components/LoginForm';
+import UserProfile from '../components/UserProfile';
+
+const SearchInput = styled(Input.Search)`
+    vertical-align: middle;
+`
+;
 
 const AppLayout = ( {children} ) => {
+    // replace with redux
+    // const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const isLogginIn = useSelector((state) => state.user.isLoggedIn)
+
 
     return (
         <div>
@@ -14,12 +28,24 @@ const AppLayout = ( {children} ) => {
                 </Menu.Item>
                 <Menu.Item>
                     <Link href="/profile"><a>Profile</a></Link>
+                </Menu.Item> 
+                <Menu.Item>
+                    {
+                        //<Input.Search enterButton style={{ verticalAlign: 'middle' }} />
+                    }
+                    <SearchInput enterButton></SearchInput>
                 </Menu.Item>
                 <Menu.Item>
                     <Link href="/signup"><a>Signup</a></Link>
                 </Menu.Item>
             </Menu>
-            {children}
+            <Row gutter={8}>
+                {/* <Col xs={24} md={6}>  {isLoggedIn ? <UserProfile setIsLoggedIn={setIsLoggedIn}/> : <LoginForm setIsLoggedIn={setIsLoggedIn}/> } </Col>  */}
+                <Col xs={24} md={6}>  {isLoggedIn ? <UserProfile /> : <LoginForm /> } </Col>
+                <Col xs={24} md={12}> {children} </Col>
+                <Col xs={24} md={6}> <a href="http://blog.naver.com/dikisun" target="_blank" rel="noreferrer noopener"> Made by DIKISUN</a> </Col>
+            </Row>
+            
         </div>
     );
 }
